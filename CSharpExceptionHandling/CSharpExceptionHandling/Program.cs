cb30972555ab7a65557f3e516a5f5b5b91830f56
jaiparmar7 <jaiparmar7@outlook.in>
2017-12-17 11:23:46 +0530

    C# exception handling best practice

---
+++ CSharpExceptionHandling/CSharpExceptionHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CSharpExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DoSomething();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            Console.ReadKey();
+        }
+
+        static void DoSomething()
+        {
+            try
+            {
+                Console.WriteLine("Inside try block");
+
+                throw new NullReferenceException();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Inside catch block");
+
+                //throw(ex) will reset your stack trace so error will appear from the line where throw(ex) written while 
+                //throw does not reset stack trace and you will get information about original exception.
+                //throw ex;
+
+                //it’s always best practice to use throw instead of throw (ex).
+                throw;
+
+            }
+            finally
+            {
+                Console.WriteLine("Inside finally block");
+            }
+        }
+    }
+}
