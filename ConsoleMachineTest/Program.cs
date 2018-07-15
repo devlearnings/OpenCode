f079bfb7be09fa4a658b691cf1369fe0a129f96a
jaiparmar7 <jaiparmar7@outlook.in>
2018-07-15 22:29:36 +0530

    Unit testing using visual studio testing tool

---
+++ ConsoleMachineTest/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleMachineTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var operation = args[0];
+            var values = string.Empty;
+
+            if (args.Length > 1)
+                values = args[1];
+
+            var result = 0;
+
+            var delimeter = GetDelimeter(values);
+            var inputValues = GetInputValue(values);
+
+            if (inputValues.Contains("Error"))
+            {
+                Console.WriteLine(inputValues);
+                Console.ReadKey();
+                return;
+            }
+
+            switch (operation)
+            {
+                case "sum":
+                case "add":
+                    result = Sum(delimeter, inputValues);
+                    break;
+                case "multiply":
+                    result = Multiply(delimeter, inputValues);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        public static char[] GetDelimeter(string values)
+        {
+            char[] delimeter = { ',', '\n' };
+
+            if (values.IndexOf("\\", StringComparison.Ordinal) == 0)
+            {
+                delimeter = values.Split('\\')[1].ToCharArray();
+            }
+
+            return delimeter;
+        }
+
+        public static string GetInputValue(string values)
+        {
+            if (values.IndexOf("\\", StringComparison.Ordinal) == 0)
+            {
+                values = values.Split('\\')[2];
+            }
+
+            if (values.Contains("-"))
+            {
+                string negativeValues = string.Empty;
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    if (values[i] == '-')
+                        negativeValues += "-" + values[i + 1] + ",";
+                }
+
+                negativeValues = negativeValues.TrimEnd(',');
+
+                values = "Error: Negative numbers (" + negativeValues + ") not allowed.";
+            }
+
+            return values;
+        }
+
+        public static int Sum(char[] delimeter, string values)
+        {
+            var valuesArray = values.Split(delimeter);
+
+            var result = 0;
+
+            var e = valuesArray.GetEnumerator();
+            while (e.MoveNext())
+            {
+                if (e.Current != null && Int32.TryParse(e.Current.ToString(), out int currValue))
+                {
+                    if (currValue < 1000)
+                        result += currValue;
+                }
+            }
+
+            return result;
+        }
+
+        public static int Multiply(char[] delimeter, string values)
+        {
+            var valuesArray = values.Split(delimeter);
+
+            var result = 1;
+
+            var e = valuesArray.GetEnumerator();
+            while (e.MoveNext())
+            {
+                if (e.Current != null && Int32.TryParse(e.Current.ToString(), out int currValue))
+                {
+                    if (currValue < 1000)
+                        result *= currValue;
+                }
+            }
+
+            return result;
+        }
+
+
+    }
+}
---
+++ UnitTests/ProgramTest.cs
@@ -0,0 +1,90 @@
+﻿using ConsoleMachineTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        private readonly char[] _delimeter = { ',', '\n' };
+
+        [TestMethod]
+        public void TestSum_Sum_NoValues_ReturnZero()
+        {
+            var expectedResult = 0;
+            var actualResult = Program.Sum(_delimeter,string.Empty);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestSum_Sum_PassVaue1_ReturnResult1()
+        {
+            var expectedResult = 1;
+            var actualResult = Program.Sum(_delimeter,"1");
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestSum_Sum_PassVaue2And3_ReturnResult5()
+        {
+            var expectedResult = 5;
+            var actualResult = Program.Sum(_delimeter,"2,3");
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestSum_Sum_PassMultipleVaue2345_ReturnResult14()
+        {
+            var expectedResult = 14;
+            var actualResult = Program.Sum(_delimeter,"2,3,4,5");
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        public void TestSum_GetInputValue_PassMulipleDelimeter_ReturnResult14()
+        {
+            var expectedResult = "2,3,4";
+            var actualResult = Program.GetInputValue("2\n3,4");
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestSum_GetDelimeter_PassDynamicDelimeter()
+        {
+            char[] expectedResult = {';'};
+            var actualResult = Program.GetDelimeter("\\;\\3;4;5 ");
+            Assert.AreEqual(expectedResult.ToString(), actualResult.ToString());
+        }
+
+        [TestMethod]
+        public void TestSum_GetInputValue_AllPositive()
+        {
+            var expectedResult = "2;3;4";
+            var actualResult = Program.GetInputValue("\\;\\2;3;4");
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestSum_GetInputValue_ContainsNegative()
+        {
+            var expectedResult = "Error: Negative numbers (-3,-5) not allowed.";
+            var actualResult = Program.GetInputValue("\\;\\2;-3;4;-5");
+            Assert.AreEqual(expectedResult.ToString(), actualResult.ToString());
+        }
+
+        [TestMethod]
+        public void TestSum_Sum_PassGreaterThan1000()
+        {
+            var expectedResult = 11;
+            var actualResult = Program.Sum(_delimeter, "2,3000,4,5");
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMultiply_Pass2And4And3_Return6()
+        {
+            var expectedResult = 24;
+            var actualResult = Program.Multiply(_delimeter, "2,4,3");
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
